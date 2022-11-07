de1a2173ea6122d8dcb3eee546eb67052a8f6ad7
Bytecoder <bytecoder0x@gmail.com>
2022-11-07 21:02:59 +0200

    config with symbol and windows

---
+++ config.py
@@ -0,0 +1,12 @@
+SYMBOL = "BTCUSDT"
+INTERVAL = "1d"  # 1h, 4h, 1d, 1w
+LIMIT = 1000  # max candles for one request on binance
+
+FAST_WINDOW = 20
+SLOW_WINDOW = 50
+VOLATILITY_WINDOW = 20
+
+START_BALANCE = 1000  # USDT
+FEE = 0.001  # 0.1% binance spot fee
+
+DATA_DIR = "data"
