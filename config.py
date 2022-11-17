a62cae9240f7b095c7162ecd6370fca85cace99e
Bytecoder <bytecoder0x@gmail.com>
2022-11-17 19:22:12 +0200

    fees and exchanges in config

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+EXCHANGES = ["binance", "kraken"]
 SYMBOL = "BTCUSDT"
 INTERVAL = "1d"  # 1h, 4h, 1d, 1w
-LIMIT = 1000  # max candles for one request on binance
+LIMIT = 720  # candles per exchange kraken give max 720
 
 FAST_WINDOW = 20
 SLOW_WINDOW = 50
 VOLATILITY_WINDOW = 20
 
 START_BALANCE = 1000  # USDT
-FEE = 0.001  # 0.1% binance spot fee
+FEES = {"binance": 0.001, "kraken": 0.0026}  # taker fee on spot
 
 DATA_DIR = "data"
